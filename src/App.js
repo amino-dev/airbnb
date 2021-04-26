064f1acb3fa78a2c9e6d2489fa8db39a74d037e7
aminoo <sametmedaminesimplonenda@gmail.com>
2021-04-27 01:09:04 +0200

    explore section

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Cities from './components/explore'
+
+function App() {
+  return (
+    <div className="">
+      <Cities/>
+    </div>
+  );
+}
+
+export default App;
